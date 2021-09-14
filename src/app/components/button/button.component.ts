669bc02f285892092858d258978fd7ebc364d6b9
Alex Olaru <>
2021-09-14 19:12:17 +0300

    update

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,17 +6,16 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
-  @Input() text: string | undefined;
-  @Input() color: string | undefined;
-  @Output() btnClick = new EventEmitter;
+  @Input() text: string;
+  @Input() color: string;
+  @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
 
   onClick() {
-    console.log("hey");
-    
+    this.btnClick.emit(); 
   }
 }
